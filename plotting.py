a2b98f9364fa1b3b0983e1b0a577f258178ffa7c
Chihoon Lee <89762947+chihoon1@users.noreply.github.com>
2024-01-21 12:31:02 -0500

    added density clustering

--- plotting.py
+++ plotting.py
@@ -14,9 +14,10 @@ def plot_clustering(D, cluster_labels, **kwargs):
     :param cluster_labels: numpy 1d array represent cluster label of data point in D
     :return: None
     '''
+    dtype = kwargs.get('dtype', 'int32')  # data type of cluster labels. Default: integer
     fig = plt.figure()
     dim = 1 if len(D.shape) == 1 else D.shape[1]
-    cluster_labels = cluster_labels.astype('int32')
+    cluster_labels = cluster_labels.astype(dtype)
     if dim == 2:
         sns.scatterplot(x=D[:, 0], y=D[:, 1], hue=cluster_labels)
     elif dim == 3:
